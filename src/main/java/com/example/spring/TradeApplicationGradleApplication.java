ddeff79d2f9df0c228c4643c8389a56992b97c45
unknown <soumya.1992.das@gmail.com>
2023-06-25 14:15:50 +0530

    soumya 25-06-2023 done postgres

--- src/main/java/com/example/spring/TradeApplicationGradleApplication.java
+++ src/main/java/com/example/spring/TradeApplicationGradleApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan("com.example.demo")
+@EnableJpaAuditing
+@ComponentScan("com.example.spring")
 public class TradeApplicationGradleApplication {
 
 	public static void main(String[] args) {
